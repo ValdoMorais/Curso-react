56670cfe5f7bfbabe87bffd48886c340fa67e3f0
Valdo Flor <82148347+ValdoMoraisFlor@users.noreply.github.com>
2022-11-30 18:40:58 +0200

    Feat: Fundamentos JSX

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const name = 'Valdo Morais'
+const newName = name.toUpperCase()
+const url = 'http://via.placeholder.com/150'
+
+function sum(a, b){
+  return a+b;
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  
+  <div className="root">
+    <h1>{name}!</h1>
+    <h2> Alternando o JSX</h2>
+    <p> Ola {newName}</p>
+    <p> A soma de 1 e 2 e: {sum(1,2)}</p>
+    <img src={url} alt="minha Imagem"/>
+  </div>
 );
 
 // If you want to start measuring performance in your app, pass a function
